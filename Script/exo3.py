3bd378b0e421141365a592b1d6462d8c6f2471bf
Escias <ljimmy98@hotmail.fr>
2021-01-28 12:25:16 +0100

    condition note between 0 and 20

--- Script/exo3.py
+++ Script/exo3.py
@@ -8,9 +8,21 @@ class ExoWhile:
             i += note
         print("moyenne : " + str(round(i / len(notes), 2)))
 
+
+
     def threeNote(self):
-        note_1, note_2, note_3 = input("Enter 3 number between 0 and 20 ").split()
-        notes = [int(note_1), int(note_2), int(note_3)]
+        i = 0
+        notes = []
+        #note_1, note_2, note_3 = input("Enter 3 number between 0 and 20 ").split()
+        while i < 3:
+            number = int(input("note " + str(i+1) + " = "))
+            if 0 <= number <= 20:
+                notes.append(number)
+            else:
+                print("Must be between 0 and 20")
+                continue
+            i += 1
+        #notes = [int(note_1), int(note_2), int(note_3)]
         self.calcul(notes)
 
     def note(self):
@@ -18,15 +30,24 @@ class ExoWhile:
         notes = []
         n = int(input("Number of notes "))
         while i < n:
-            notes.append(int(input("note " + str(i+1) + " = ")))
+            number = int(input("note " + str(i + 1) + " = "))
+            if 0 <= number <= 20:
+                notes.append(number)
+            else:
+                print("Must be between 0 and 20")
+                continue
             i += 1
         print(notes)
         self.calcul(notes)
 
     def noEnd(self):
         notes = []
-        inp = input("Enter a note or type 'fin' ")
-        while inp != "fin":
-            notes.append(int(inp))
-            inp = input("Enter a new note or type 'fin' ")
+        number = input("Enter a note or type 'fin' ")
+        while number != "fin":
+            n = int(number)
+            if 0 <= n <= 20:
+                notes.append(n)
+            else:
+                print("Must be between 0 and 20")
+            number = input("Enter a new note or type 'fin' ")
         self.calcul(notes)
